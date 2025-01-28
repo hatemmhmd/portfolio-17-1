72b6775b9f62a9330270e39dd6059c99ab43ada9
Hatem Alhusheky <Hatem.Alhushkey@infinite-tek.com>
2025-01-29 02:38:30 +0300

    Add Contact Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,13 @@ import AOS from "aos";
 import { NgClass, NgIf } from '@angular/common';
 import { AboutComponent } from './about/about-me/about.component';
 import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ,HomeComponent , NgIf , NgClass , AboutComponent , ServicesComponent],
+  imports: [RouterOutlet ,HomeComponent,NgIf,NgClass,
+  AboutComponent,ServicesComponent,SkillsComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
