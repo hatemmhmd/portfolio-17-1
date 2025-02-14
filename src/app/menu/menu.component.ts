5b076b9608547746a18ddc45f3a7606982d61277
Hatem Alhusheky <Hatem.Alhushkey@infinite-tek.com>
2025-02-15 01:23:09 +0300

    test-hover

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,12 +10,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class MenuComponent {
 
+  isHovering : boolean = false;
+
   @Input() clickMenu : boolean | undefined; 
   @Output() viewMenu = new EventEmitter<boolean>();
   @Output() scrollsection = new EventEmitter<string>();
 
 
-  onListMenu(section : string) {
+    onListMenu(section : string) {
     this.viewMenu.emit(this.clickMenu);
     this.scrollsection.emit(section);
   }
